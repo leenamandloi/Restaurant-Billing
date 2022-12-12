c535a346f4f9c2042a0e95123922b7217c1d1370
leenamandloi <leenamandloi29@gmail.com>
2022-12-12 14:32:12 +0530

    first commit

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,5 @@
+generateBillHeader(ord.customer,ord.date);
+// for(int i=0;i<ord.numOfItems;i++){
+//     generateBillBody(ord.itm[i].item,ord.itm[i].qty,ord.itm[i].price);
+// }
+// generateBillFooter(total);
